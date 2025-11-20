5c34d1362fe1fd65863810b6c3218b64f638d572
Puneet Jain <jainpuneet711@gmail.com>
2025-11-20 20:08:48 +0530

    minor error resolved in user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,4 +6,5 @@ const userSchema = new mongoose.Schema({
   authProvider: String,
 });
 
-export default User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
